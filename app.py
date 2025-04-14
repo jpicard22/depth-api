66fd4e44b8f41e521ac142cbbc1af13f9e787be4
jpicard22 <jey22100@gmail.com>
2025-04-14 21:06:28 +0200

    pret pour railway23

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def depth():
         return f"Erreur serveur : {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))  # 🚀 ici on récupère le port de Railway
+    app.run(host="0.0.0.0", port=port)

f193cf9b0da6264a9d00cac999874664b3dd41bc
jpicard22 <jey22100@gmail.com>
2025-04-14 16:51:50 +0200

    pret pour railway5

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def check_file(filename):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+

04ca8ed002d401a47a70f3397b044a8d96354896
jpicard22 <jey22100@gmail.com>
2025-04-16 17:30:59 +0200

    perso10 + loader9

--- midas/model_loader.py
+++ midas/model_loader.py
@@ -1,13 +1,19 @@
 import torch
 from midas.midas_net import MidasNet_small
+from torchvision import models
 
 def load_model(model_type: str, device: torch.device, model_path: str):
     if model_type == "midas_v21_small_256":
-        # Retirer l'argument exportable
+        # Charger ResNet50 localement
+        resnet50 = models.resnet50(pretrained=False)  # Ne pas télécharger les poids par défaut
+        resnet50.load_state_dict(torch.load(model_path))  # Charger les poids depuis le fichier local
+        resnet50.to(device)
+        
+        # Maintenant, initialiser MidasNet_small avec ResNet50 comme backbone
         model = MidasNet_small(
             path=model_path,
             features=64,
-            backbone="efficientnet_lite3",
+            backbone=resnet50,  # Utiliser le ResNet50 chargé localement
             non_negative=True,
             blocks={'expand': True}
         )

cdf9fdd6bde9f429892da068465d133d51f8c240
jpicard22 <jey22100@gmail.com>
2025-04-16 16:55:37 +0200

    perso10 + loader4

--- midas/model_loader.py
+++ midas/model_loader.py
@@ -3,9 +3,17 @@ from midas.midas_net import MidasNet_small
 
 def load_model(model_type: str, device: torch.device, model_path: str):
     if model_type == "midas_v21_small_256":
-        model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3", exportable=False, non_negative=True, blocks={'expand': True})
+        model = MidasNet_small(
+            path=model_path,
+            features=64,
+            backbone="efficientnet_lite3",
+            exportable=False,
+            non_negative=True,
+            blocks={'expand': True}
+        )
     else:
         raise ValueError(f"Model type {model_type} not supported.")
 
     model.to(device)
+    model.eval()
     return model

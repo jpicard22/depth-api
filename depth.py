78061b717805bf2b78d8bc387fda337cf6057a59
jpicard22 <jey22100@gmail.com>
2025-04-17 14:52:05 +0200

    re28

--- depth.py
+++ depth.py
@@ -7,7 +7,7 @@ import logging
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'midas'))
 
-from midas_net_custom import MidasNet_small
+from midas.midas_net_custom import MidasNet_small
 # from midas_net import MidasNet_small
 from transforms import Resize, NormalizeImage, PrepareForNet
 from torchvision.transforms import Compose
